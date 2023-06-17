bab5265fa83df0e4ae1ad054a7dff378d046e8e6
camm852 <camm852@gmail.com>
2023-06-16 21:19:19 -0500

    Init project

---
+++ src/main/java/com/software/views/FtpClient.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.software.views;
+
+/**
+ *
+ * @author camm0
+ */
+public class FtpClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
